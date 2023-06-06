a72c20d3a9ccf6880378e12609d407ec37ae69b9
Oscar <oscar@r-digital.tech>
2023-06-05 20:41:47 -0600

    add navegacion.cy.js

---
+++ cypress/e2e/navegacion.cy.js
@@ -0,0 +1,26 @@
+describe('Navegacion', () => {
+	it('Navegar a nuestra primer pagina', () => {
+		cy.visit('www.platzi.com')
+	})
+
+	it('Recargar pagina', () => {
+        cy.visit('www.platzi.com')
+		cy.reload()
+	})
+
+	it('Recargar pagina de forma forzada', () => {
+        cy.visit('www.google.com')
+		cy.reload(true)
+	})
+
+    it.only('Navegar hacia atras y hacia adelante', () => {
+        cy.visit('www.google.com')
+        cy.visit('https://www.google.com/search?q=platzi&oq=platzi&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTISCAEQLhhDGMcBGLEDGNEDGIoFMgYIAhBFGDwyBggDEEUYPDIGCAQQRRhBMgYIBRBFGEEyBggGEEUYQTIGCAcQRRg80gEIMTc1OWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8')
+		
+        cy.go('back')
+        // cy.go(-1)
+
+        cy.go('forward')
+        // cy.go(+1)
+	})
+})
